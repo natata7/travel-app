9ec9f39d40a7f6a3be1d3e8dae19251f51187727
natata7 <natata7@gmail.com>
2022-06-11 18:33:05 +0300

    add simple login functional to app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import './App.css';
+import "./App.css";
 import { Route, Navigate, Routes } from "react-router-dom";
-import Home from './components/HomeScreen/Home';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-// import SingUp from './components/SingUpScreen';
-// import SingIn from './components/SingInScreen';
-// import Bookings from './components/BookingsScreen';
-// import TripInfoScreen from './components/TripInfoScreen';
+import Home from "./components/HomeScreen/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import SingUp from "./components/SignUpScreen/SignUp";
+import SingIn from "./components/SignInScreen/SignIn";
+import Bookings from "./components/BookingsScreen/Bookings";
+import TripPage from "./components/TripInfoScreen/TripPage";
+import { useState } from "react";
 
 function App() {
+  const [isLoggedin, setIsLoggedin] = useState(false);
+  console.log()
+  function handleLoggedinClick() {
+    setIsLoggedin(true);
+  }
+
   return (
     <div className="App">
-      <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/sing-up" element={<SingUp />} />
-          <Route path="/sing-in" element={<SingIn />} />
-          <Route path="/bookings" element={<Bookings />} />
-          <Route path="/trip/:tripId" element={<TripInfoScreen />} />
-           */}
-           <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-        <Footer />
+      <Header isLoggedIn={isLoggedin} onLoggedOut={setIsLoggedin} />
+      <Routes>
+        <Route
+          path="/"
+          element={isLoggedin ? <Home /> : <Navigate to="/sign-up" />}
+        />
+        <Route
+          path="bookings"
+          element={isLoggedin ? <Bookings /> : <Navigate to="/sign-up" />}
+        />
+        <Route path="sign-up" element={<SingUp onSubmit={setIsLoggedin} />} />
+        <Route path="sign-in" element={<SingIn onSubmit={setIsLoggedin} />} />
+        <Route path="trip/:tripId" element={<TripPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
