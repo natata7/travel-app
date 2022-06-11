86434a0b50f3e9290a33e272a279b1f13f8be65c
natata7 <natata7@gmail.com>
2022-06-11 18:31:45 +0300

    add loader to trip page

--- src/components/TripInfoScreen/TripPage.jsx
+++ src/components/TripInfoScreen/TripPage.jsx
@@ -2,6 +2,7 @@ import './TripPage.css';
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getTripDetails } from "../../helpers/tripService";
+import Loader from "../Loader/loader";
 
 function TripPage() {
   const { tripId } = useParams();
@@ -25,28 +26,34 @@ function TripPage() {
       );
   }, []);
 
-  return (
-    <main className="trip-page">
-      <h1 className="visually-hidden">Travel App</h1>
-      <div className="trip">
-        <img src={trip.image} className="trip__img" alt={trip.title} />
-        <div className="trip__content">
-          <div className="trip-info">
-            <h3 className="trip-info__title">{trip.title}</h3>
-            <div className="trip-info__content">
-              <span className="trip-info__duration"><strong>{trip.duration}</strong> days</span>
-              <span className="trip-info__level">{trip.level}</span>
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <Loader />;
+  } else {
+    return (
+      <main className="trip-page">
+        <h1 className="visually-hidden">Travel App</h1>
+        <div className="trip">
+          <img src={trip.image} className="trip__img" alt={trip.title} />
+          <div className="trip__content">
+            <div className="trip-info">
+              <h3 className="trip-info__title">{trip.title}</h3>
+              <div className="trip-info__content">
+                <span className="trip-info__duration"><strong>{trip.duration}</strong> days</span>
+                <span className="trip-info__level">{trip.level}</span>
+              </div>
             </div>
+            <div className="trip__description">{trip.description}</div>
+            <div className="trip-price">
+              <span>Price</span>
+              <strong className="trip-price__value">{trip.price} $</strong>
+            </div>
+            <button className="trip__button button">Book a trip</button>
           </div>
-          <div className="trip__description">{trip.description}</div>
-          <div className="trip-price">
-            <span>Price</span>
-            <strong className="trip-price__value">{trip.price} $</strong>
-          </div>
-          <button className="trip__button button">Book a trip</button>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 export default TripPage;
