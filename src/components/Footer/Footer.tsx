cca4b31bbb3e0ba765f98149bf80927f20a05490
natata7 <natata7@gmail.com>
2022-06-13 00:36:37 +0300

    fix icon in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
         <footer className="footer">
         <span className="footer__text">
             from <a className="footer__link" href="https://binary-studio.com">binary studio</a> with
-            <img className="footer__icon" src="./images/heart.svg" alt="heart icon" />
+            <img className="footer__icon" src="/images/heart.svg" alt="heart icon" />
         </span>
         </footer>
     );
