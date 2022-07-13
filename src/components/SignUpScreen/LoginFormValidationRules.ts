151863591e00bf6d59d9c17b2bf2d41e7d69e1a8
natata7 <natata7@gmail.com>
2022-07-13 20:03:36 +0300

    add password validation on signup

--- src/components/SignUpScreen/LoginFormValidationRules.ts
+++ src/components/SignUpScreen/LoginFormValidationRules.ts
@@ -3,6 +3,9 @@ import IForm from "../../interfaces/Form.interface";
 
 export default function validate(values: IForm) {
     let errors:IErrors = {};
+    const numbers = /[0-9]/g;
+    const upperCaseLetters = /[A-Z]/g;
+    const lowerCaseLetters = /[a-z]/g;
     if (!values.fullName) {
         errors.fullName = 'Full name is required';
       } else if (values.fullName.length < 3) {
@@ -17,6 +20,12 @@ export default function validate(values: IForm) {
       errors.password = 'Password is required';
     } else if (values.password.length < 3 || values.password.length > 20) {
       errors.password = 'Password must be 3 - 20 characters';
+    } else if(!values.password.match(numbers)) {
+      errors.password = 'Password must contain a number';
+    } else if(!values.password.match(upperCaseLetters)) {
+      errors.password = 'Password must contain a uppercase letter';
+    } else if(!values.password.match(lowerCaseLetters)) {
+      errors.password = 'Password must contain a lovercase letter';
     }
     return errors;
   };
